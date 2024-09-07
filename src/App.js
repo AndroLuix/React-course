976dc7e0f56a4cfc58012b0a7ee5c60ebf6fd299
luigi iadicola <luigiadicola@gmail.com>
2024-09-07 19:00:56 +0200

    props and map

--- src/App.js
+++ src/App.js
@@ -4,19 +4,52 @@ import Componente1 from './Componente1';
 import 'bootstrap/dist/css/bootstrap.css';
 import Product from './Product';
 
+const Products = [
+  {
+    prezzo: "10,00",
+    nome: "PROMAVERIX",
+    image: "https://th.bing.com/th/id/R.4fa63f127ec1a820b0c95ceb1f88a015?rik=E4mb48miX5RBHQ&pid=ImgRaw&r=0",
+  },
+  {
+    prezzo: "15,00",
+    nome: "MENO-15",
+    image: "https://th.bing.com/th/id/OIP.V761uo8rBp5wlJn9PgoxjAAAAA?rs=1&pid=ImgDetMain",
+  },
+  {
+    prezzo: "10,00",
+    nome: "PROMAVERIX",
+    image: "https://th.bing.com/th/id/R.4fa63f127ec1a820b0c95ceb1f88a015?rik=E4mb48miX5RBHQ&pid=ImgRaw&r=0",
+  },
+  {
+    prezzo: "15,00",
+    nome: "MENO-15",
+    image: "https://th.bing.com/th/id/OIP.V761uo8rBp5wlJn9PgoxjAAAAA?rs=1&pid=ImgDetMain",
+  },
+    {
+    prezzo: "10,00",
+    nome: "PROMAVERIX",
+    image: "https://th.bing.com/th/id/R.4fa63f127ec1a820b0c95ceb1f88a015?rik=E4mb48miX5RBHQ&pid=ImgRaw&r=0",
+  },
+  {
+    prezzo: "15,00",
+    nome: "MENO-15",
+    image: "https://th.bing.com/th/id/OIP.V761uo8rBp5wlJn9PgoxjAAAAA?rs=1&pid=ImgDetMain",
+  }
+];
 
 function App() {
+
+  const PrdocutList = Products.map((productElem) => {
+    return <Product {...productElem}></Product>;
+  });
+
   return (
-  <section className='container-xxl d-flex flex-row gap-3 flex-wrap justify-content-center'>
-  <Product/>
-  <Product/>
-  <Product/>
-  <Product/>
-  <Product/>
-  <Product/>
-  <Product/>
-    {/* <Componente1/> */}
-  </section>
+    <section className='container-xxl d-flex flex-row gap-3 flex-wrap justify-content-center mt-5'>
+
+      {PrdocutList}
+
+      {/* <Componente1/> */}
+    </section>
   );
 }
 
--- src/Product.js
+++ src/Product.js
@@ -1,18 +1,22 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css';
-const img = "https://th.bing.com/th/id/R.4fa63f127ec1a820b0c95ceb1f88a015?rik=E4mb48miX5RBHQ&pid=ImgRaw&r=0";
-const alt = 'alt img';
 
-const prezzo = "14.00";
 
-const Product = () => {
+const myStyle = {
+  backgroundColor: "DodgerBlue",
+  padding: "5px",
+  width: '200px', height: '200px',
+  margin: 'auto',
+};
+
+const Product = ({image, nome, prezzo, childern}) => {
   return (
     <article className="col-md-3 mb-4">
       <div className='card'>
-        <img src={img} alt={alt} className="card-img-top" />
+        <img src={image}  className="card-img-top" style={myStyle} />
         <div className="card-body">
-          <h6 className="card-title">10 Euro Coupon</h6>
+          <h6 className="card-title">{nome} Coupon</h6>
           <hr />
+          <p className='p-2 '>{childern}</p>
           <div className="d-flex justify-content-between align-items-center">
             <span className="text-muted">{prezzo} €</span>
             <button className="btn btn-primary">Acquista</button>
